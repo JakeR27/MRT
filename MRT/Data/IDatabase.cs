664aa306ce5c0bdcbcc8effefe90f2530367f9cf
JakeR27 <dev@jakebs.xyz>
2023-05-20 21:50:22 +0100

    updated database interface to specify type on insert

--- MRT/Data/IDatabase.cs
+++ MRT/Data/IDatabase.cs
@@ -5,7 +5,7 @@ namespace MRT.Data;
 
 public interface IDatabase
 {
-    public void Insert<TData>(TData record, string collection);
+    public void Insert<TData>(TData record, string collection) where TData : IModel;
     public TData? Get<TData>(Guid id, string collection) where TData : IModel;
     public List<TData> Get<TData>(string collection);
     public List<TData> Get<TData>(string collection, Expression<Func<TData, bool>> predicate);

51d7e00451cb65ff5926f86a70d1bf034d144b57
JakeR27 <dev@jakebs.xyz>
2023-05-20 21:55:49 +0100

    updated database interface to return bool on insert

--- MRT/Data/IDatabase.cs
+++ MRT/Data/IDatabase.cs
@@ -5,7 +5,7 @@ namespace MRT.Data;
 
 public interface IDatabase
 {
-    public void Insert<TData>(TData record, string collection) where TData : IModel;
+    public bool Insert<TData>(TData record, string collection) where TData : IModel;
     public TData? Get<TData>(Guid id, string collection) where TData : IModel;
     public List<TData> Get<TData>(string collection) where TData : IModel;
     public List<TData> Get<TData>(string collection, Expression<Func<TData, bool>> predicate) where TData : IModel;

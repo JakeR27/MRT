0f4f583387ad9d66b9b3c36fc04562f22c1dc895
JakeR27 <dev@jakebs.xyz>
2023-05-20 21:51:06 +0100

    updated database interface to specify type when either get method used

--- MRT/Data/IDatabase.cs
+++ MRT/Data/IDatabase.cs
@@ -7,7 +7,7 @@ public interface IDatabase
 {
     public void Insert<TData>(TData record, string collection) where TData : IModel;
     public TData? Get<TData>(Guid id, string collection) where TData : IModel;
-    public List<TData> Get<TData>(string collection);
-    public List<TData> Get<TData>(string collection, Expression<Func<TData, bool>> predicate);
+    public List<TData> Get<TData>(string collection) where TData : IModel;
+    public List<TData> Get<TData>(string collection, Expression<Func<TData, bool>> predicate) where TData : IModel;
     public bool Update<TData>(TData record, string collection) where TData : IModel;
 }

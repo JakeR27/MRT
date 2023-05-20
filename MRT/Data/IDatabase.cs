46574ad061a2feb0d5ee4eed264c448a230bb779
JakeR27 <dev@jakebs.xyz>
2023-05-20 21:32:26 +0100

    added a database interface

---
+++ MRT/Data/IDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using MRT.Data.ResultModels;
+
+namespace MRT.Data;
+
+public interface IDatabase
+{
+    public void Insert<TData>(TData record, string collection);
+    public TData? Get<TData>(Guid id, string collection) where TData : IModel;
+    public List<TData> Get<TData>(string collection);
+    public List<TData> Get<TData>(string collection, Expression<Func<TData, bool>> predicate);
+    public bool Update<TData>(TData record, string collection) where TData : IModel;
+}

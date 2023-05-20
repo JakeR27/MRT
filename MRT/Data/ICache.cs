4d1f1350712829b9a5502ce2dd02e0c49a727c1e
JakeR27 <dev@jakebs.xyz>
2023-05-20 21:42:29 +0100

    defined ICache for instances of caching objects

---
+++ MRT/Data/ICache.cs
@@ -0,0 +1,8 @@
+﻿namespace MRT.Data;
+
+public interface ICache
+{
+    public TData Get<TData>(Guid id);
+    public void Insert<TData>(TData record);
+    public void Update<TData>(TData record);
+}

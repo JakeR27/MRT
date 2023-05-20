7bbda8f0bc9129cdea95b6ab4ea4182d0f1d65ec
JakeR27 <dev@jakebs.xyz>
2023-05-20 21:51:58 +0100

    updated litedb implementation with database interface update

--- MRT/Data/LiteDb.cs
+++ MRT/Data/LiteDb.cs
@@ -22,7 +22,7 @@ public class LiteDb : IDatabase
         return _instance ??= new LiteDb();
     }
 
-    public void Insert<TData>(TData record, string collection)
+    public void Insert<TData>(TData record, string collection) where TData : IModel
     {
         Instance().Database.GetCollection<TData>(collection).Insert(record);
     }
@@ -32,12 +32,12 @@ public class LiteDb : IDatabase
         return Instance().Database.GetCollection<TData>(collection).FindOne(record => record.Id == id);
     }
 
-    public List<TData> Get<TData>(string collection)
+    public List<TData> Get<TData>(string collection) where TData : IModel
     {
         return Instance().Database.GetCollection<TData>(collection).FindAll().ToList();
     }
 
-    public List<TData> Get<TData>(string collection, Expression<Func<TData, bool>> predicate)
+    public List<TData> Get<TData>(string collection, Expression<Func<TData, bool>> predicate) where TData : IModel
     {
         return Instance().Database.GetCollection<TData>(collection).Find(predicate).ToList();
     }
